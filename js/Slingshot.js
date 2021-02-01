11717b79ab2a8f7f59f68195f1d256f4ae2d9338
AKA-creator <74938084+AKA-creator@users.noreply.github.com>
2021-01-31 20:52:18 -0600

    Add files via upload

---
+++ js/Slingshot.js
@@ -0,0 +1,31 @@
+class Slingshot{
+    constructor(bodyA,pointB)
+    {
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length:2
+        }
+
+        this.Slingshot=Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world,this.Slingshot)
+    }
+  fly()
+  {
+      this.Slingshot.bodyA=null;
+  }
+    display()
+    {
+        if(this.Slingshot.bodyA)
+        {
+        var pointA = this.Slingshot.bodyA.position;
+        var pointB = this.pointB;
+
+        strokeWeight(4);
+        stroke("purple");
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+    }
+}
